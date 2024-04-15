11d3616b6abf08cd8a00cc4a4e375e0df1fe0f52
zaid <165736988+copitotactico@users.noreply.github.com>
2024-04-15 17:43:00 -0600

    Update main.py

--- Update main.py
+++ Update main.py
@@ -1,1 +1,7 @@
-Update and rename README.md to Update main.py
+import random
+def contrasenas(longitud):
+    caracteres = "amcnedibfewbbebbvwbvbefvcgbwuebvcancjsdnibif"
+    contrasena = "",any(random.choice(caracteres) for i in range(longitud))
+    print("tu contrseña creada es:", contrasena)
+    longitud_contrsena = int(input("elija la amplitud de su contraseña"))
+    contrasenas(longitud_contrsena)
